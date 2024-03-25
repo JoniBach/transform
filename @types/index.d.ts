4c3ecba2e461a71fbbadc880e311a3e6aee645c3
James <james.crook@fourthfloor.co>
2024-03-25 16:42:30 +0000

    adding default exports

---
+++ @types/index.d.ts
@@ -0,0 +1,19 @@
+import * as basic from './basic';
+import * as color from './color';
+import * as compositeData from './compositeData';
+import * as dataStructure from './dataStructure';
+import * as date from './date';
+import * as numeric from './numeric';
+import * as siUnits from './siUnits';
+import * as string from './string';
+declare const _default: {
+    basic: typeof basic;
+    color: typeof color;
+    compositeData: typeof compositeData;
+    dataStructure: typeof dataStructure;
+    date: typeof date;
+    numeric: typeof numeric;
+    siUnits: typeof siUnits;
+    string: typeof string;
+};
+export default _default;
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as basic from './basic';
+import * as color from './color';
+import * as compositeData from './compositeData';
+import * as dataStructure from './dataStructure';
+import * as date from './date';
+import * as numeric from './numeric';
+import * as siUnits from './siUnits';
+import * as string from './string';
+
+export default {
+    basic,
+    color,
+    compositeData,
+    dataStructure,
+    date,
+    numeric,
+    siUnits,
+    string,
+};
